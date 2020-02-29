a8c55128ae807d2abdd00dcdb702b7036712a5a4
Joseph Ledbetter <josephb.ledbetter@gmail.com>
2020-02-29 10:32:09 -0500

    comment out db table creation

--- db/herokuedb.sql
+++ db/herokuedb.sql
@@ -8,10 +8,10 @@ USE burgers_db;
 -- creating the table that will be inputting the burgers created 
 DROP TABLE IF EXISTS burgers;
 
-CREATE TABLE burgers (
-    id INTEGER(3) AUTO_INCREMENT NOT NULL,
-    name VARCHAR(50) NOT NULL,
-    devoured BOOLEAN NOT NULL,
-    date DATETIME NOT NULL,
-    PRIMARY KEY (id)
-);
+-- CREATE TABLE burgers (
+--     id INTEGER(3) AUTO_INCREMENT NOT NULL,
+--     name VARCHAR(50) NOT NULL,
+--     devoured BOOLEAN NOT NULL,
+--     date DATETIME NOT NULL,
+--     PRIMARY KEY (id)
+-- );
