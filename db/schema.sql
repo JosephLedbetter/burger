24725ca7c6412a3add37d2cc19fa0d44fb15d4b0
Joseph Ledbetter <josephb.ledbetter@gmail.com>
2020-02-29 10:21:08 -0500

    heroku test

--- db/schema.sql
+++ db/schema.sql
@@ -1,20 +1,20 @@
--- creating the burgers_db to hold the burgers table
-DROP DATABASE IF EXISTS burgers_db;
+-- -- creating the burgers_db to hold the burgers table
+-- DROP DATABASE IF EXISTS burgers_db;
 
-CREATE DATABASE burgers_db;
+-- CREATE DATABASE burgers_db;
 
-USE burgers_db;
+-- USE burgers_db;
 
--- creating the table that will be inputting the burgers created 
-DROP TABLE IF EXISTS burgers;
+-- -- creating the table that will be inputting the burgers created 
+-- DROP TABLE IF EXISTS burgers;
 
-CREATE TABLE burgers (
-    id INTEGER(3) AUTO_INCREMENT NOT NULL,
-    name VARCHAR(50) NOT NULL,
-    devoured BOOLEAN NOT NULL,
-    date DATETIME NOT NULL,
-    PRIMARY KEY (id)
-);
+-- CREATE TABLE burgers (
+--     id INTEGER(3) AUTO_INCREMENT NOT NULL,
+--     name VARCHAR(50) NOT NULL,
+--     devoured BOOLEAN NOT NULL,
+--     date DATETIME NOT NULL,
+--     PRIMARY KEY (id)
+-- );
 
 
 
